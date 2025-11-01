0c6a85e77772107edba581c96819222a9cc9d741
kbinwal7 <khusheebinwal29@gmail.com>
2025-11-01 16:25:11 +0530

    some changes in app

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify, request
-from src.helper import download_hugging_face_embeddings
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+from helper import download_hugging_face_embeddings
+
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
